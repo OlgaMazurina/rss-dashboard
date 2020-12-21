db0d3409513dae04a111f73316f98b8c1710c01b
Ivan <raggamix@mail.ru>
2020-12-21 17:45:27 +0300

    feat: add current date in footer

--- src/index.js
+++ src/index.js
@@ -5,9 +5,18 @@ import { createChart } from './chart.js';
 import { createMap } from './map.js';
 import { createTotalStat } from './totalCases.js';
 
+const getTodayDate = () => {
+    let months = [ "January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December" ];
+
+    document.querySelector('.footer_year').innerHTML =
+    new Date().getDate() + " " + months[new Date().getMonth()] + ' 2020';
+}
+
 window.onload = () => {
     document.body.append(new App().init());
     createChart();
     createMap();
     createTotalStat();
+    getTodayDate();
 }
